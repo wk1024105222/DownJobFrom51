c15e0c25b439200af52506bb52087cf8956337cd
wk1024105222 <1024105222@qq.com>
2017-03-16 13:20:43 +0800

    添加数据库导出/入脚本
    添加 数据库 导出 导入 脚本

---
+++ script/expimp.cmd
@@ -0,0 +1,7 @@
+exp wkai/wkai@wkai file=job51.dmp log=job51.log tables=(job51)
+
+imp wkai/wkai file=job51.dmp log=imp.log fromuser=wkai TABLES=(job51)
+
+db version :oracle Release 11.2.0.1.0 - Production
+
+
